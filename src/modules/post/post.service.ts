a26aaf03a6044efb9f0276ef4c008cc291ed3a17
FaizulOsman <faizulosmanrabby@gmail.com>
2023-08-14 00:13:17 +0600

    feat: population/include functionality added

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -3,7 +3,13 @@ import { Post, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const createPost = async (data: Post): Promise<Post> => {
-  const result = await prisma.post.create({ data });
+  const result = await prisma.post.create({
+    data,
+    include: {
+      author: true,
+      category: true,
+    },
+  });
 
   return result;
 };
